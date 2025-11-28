c4594baa28281b1534a330f28355054c02811b7d
ikromovvv <isardor859@gmail.com>
2025-11-28 10:59:01 +0500

    fsd

--- app/[category]/[id]/page.tsx
+++ app/[category]/[id]/page.tsx
@@ -1,23 +1,22 @@
 "use client"
 
 import Header from "@/components/header"
-import {useParams} from "next/navigation"; // agar header bor bo‘lsa
+import {useParams} from "next/navigation";
+import {MovieDetail} from "@/app/[category]/[id]/movie-detail";
+
+
 
 export default function ProfilePage() {
 
 
     const { id} = useParams()
-    console.log(id)
     return (
         <div className="min-h-screen bg-background text-foreground">
             {/* Header */}
             {/*<Header />*/}
 
             {/* Main Content */}
-            <div className="flex flex-col items-center justify-center h-[calc(100vh-80px)]">
-                <h1 className="text-3xl font-bold mb-4">Profile</h1>
-                <p className="text-lg text-muted-foreground">Welcome to your profile page! {id}</p>
-            </div>
+                <MovieDetail id={id}/>
         </div>
     )
 }
